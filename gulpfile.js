0e6451f10454979de7a2bde8af5517c2f659897d
Ken Hawkins <khawkins98@gmail.com>
2020-10-14 16:43:06 +0200

    Optimise local file copying, building

--- gulpfile.js
+++ gulpfile.js
@@ -14,11 +14,11 @@ require('./node_modules/\@visual-framework/vf-extensions/gulp-tasks/_gulp_rollup
 gulp.task('watch', function() {
   gulp.watch(['./src/components/**/*.scss', '!./src/components/**/package.variables.scss'], gulp.parallel('vf-css'));
   gulp.watch(['./src/components/**/*.js'], gulp.parallel('vf-scripts'));
-  gulp.watch(['./src/pages/**/*'], gulp.series('pages', 'fileinclude'));
+  gulp.watch(['./src/pages/**/*'], gulp.parallel('build-assets', 'build-html'));
   gulp.watch(['./build/**/*'], gulp.series('browser-reload'));
 });
 
-gulp.task('fileinclude', function(done) {
+gulp.task('build-html', function(done) {
   gulp.src('./src/pages/**/*.html') // important: only on html files
     .pipe(fileinclude({
       prefix: '@@',
@@ -28,9 +28,9 @@ gulp.task('fileinclude', function(done) {
   done();
 });
 
-// Copy pages to the build directory
-gulp.task('pages', function(){
-  return gulp.src('./src/pages/**/*')
+// Copy local assets to the build directory
+gulp.task('build-assets', function() {
+  return gulp.src(['./src/pages/**/*','!./src/pages/**/*.html'])
     .pipe(gulp.dest(buildDestionation));
 });
 
@@ -53,15 +53,13 @@ gulp.task('browser-reload', function(done) {
 // Let's build this sucker.
 gulp.task('build', gulp.series(
   'vf-clean',
-  gulp.parallel('pages','vf-css','vf-scripts','vf-component-assets'),
-  'fileinclude'
+  gulp.parallel('build-assets','build-html','vf-css','vf-scripts','vf-component-assets')
 ));
 
 // Build and watch things during dev
 gulp.task('dev', gulp.series(
   'vf-clean',
-  gulp.parallel('pages','vf-css','vf-scripts','vf-component-assets'),
-  'fileinclude',
+  gulp.parallel('build-assets','build-html','vf-css','vf-scripts','vf-component-assets'),
   'browser-sync',
   gulp.parallel('watch','vf-watch')
 ));
