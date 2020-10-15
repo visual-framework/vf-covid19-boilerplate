51a2524033cc4c7f9162f2281c9052f4b9f9c7df
Ken Hawkins <khawkins98@gmail.com>
2020-10-15 13:35:46 +0200

    Enhance documentation and onboarding (#10)
    * Update README.md
    
    * Update components
    
    * Fix browser-sync path
    
    * Add vf-section-header
    
    * Add vf-card
    
    * Fix image paths
    
    * Don't write image files twice
    
    * Use vf-extensions and vf-config
    
    * Remove legacy fractal build modes
    
    * Don't perform fileinclude on images
    
    * Updaate watch
    
    * Fix scripts include
    
    * Update README.md
    
    * Use VF to include local CSS overrides
    
    * Optimise local file copying, building
    
    * No need for explicit callback
    
    * Remove redundant watches
    
    These are handled by vf-watch
    
    * Add vf-summary
    
    * More example text
    
    * Add vf-u-fullbleed
    
    * Pin components

--- gulpfile.js
+++ gulpfile.js
@@ -1,115 +1,43 @@
-const gulp  = require('gulp');
-const browserSync = require('browser-sync').create();
-var   fractalBuildMode;
-// -----------------------------------------------------------------------------
-// Configuration
-// -----------------------------------------------------------------------------
-
-// Pull in some optional configuration from the package.json file, a la:
-// "vfConfig": {
-//   "vfName": "My Component Library",
-//   "vfNameSpace": "myco-",
-//   "vfComponentPath": "./src/components",
-//   "vfComponentDirectories": [
-//      "vf-core-components",
-//      "../node_modules/your-optional-collection-of-dependencies"
-//     NOTE: Don't forget to symlink: `cd components` `ln -s ../node_modules/your-optional-collection-of-dependencies`
-//    ],
-//   "vfBuildDestination": "./build",
-//   "vfThemePath": "@frctl/mandelbrot"
-// },
-// all settings are optional
-// todo: this could/should become a JS module
-const fs = require('fs');
 const path = require('path');
+const gulp  = require('gulp');
 const fileinclude = require('gulp-file-include');
-const config = JSON.parse(fs.readFileSync('./package.json'));
-const vfCoreConfig = JSON.parse(fs.readFileSync(require.resolve('@visual-framework/vf-core/package.json')));
-config.vfConfig = config.vfConfig || [];
-global.vfName = config.vfConfig.vfName || "Visual Framework 2.0";
-global.vfNamespace = config.vfConfig.vfNamespace || "vf-";
-global.vfComponentPath = config.vfConfig.vfComponentPath || path.resolve('.', __dirname + '/components');
-global.vfBuildDestination = config.vfConfig.vfBuildDestination || __dirname + '/temp/build-files';
-global.vfThemePath = config.vfConfig.vfThemePath || './tools/vf-frctl-theme';
-global.vfVersion = vfCoreConfig.version || 'not-specified';
-const componentPath = path.resolve('.', global.vfComponentPath).replace(/\\/g, '/');
-const componentDirectories = config.vfConfig.vfComponentDirectories || ['vf-core-components'];
-const buildDestionation = path.resolve('.', global.vfBuildDestination).replace(/\\/g, '/');
+const browserSync = require('browser-sync').create();
 
-// The directory the site will be deployed to (if any)
-const deployDirectory = config.vfConfig.vfDeployDirectory || "vf-covid19-boilerplate";
+// Pull in optional configuration from the package.json file, a la:
+const {componentPath, componentDirectories, buildDestionation} = require('@visual-framework/vf-config');
 
 // Tasks to build/run vf-core component system
 require('./node_modules/\@visual-framework/vf-core/gulp-tasks/_gulp_rollup.js')(gulp, path, componentPath, componentDirectories, buildDestionation);
+require('./node_modules/\@visual-framework/vf-extensions/gulp-tasks/_gulp_rollup.js')(gulp, path, componentPath, componentDirectories, buildDestionation);
 
-// Watch folders for changess
+// Watch folders for changes
 gulp.task('watch', function() {
-  gulp.watch(['./src/components/**/*.scss', '!./src/components/**/package.variables.scss'], gulp.parallel('vf-css'));
-  gulp.watch(['./src/components/**/*.js'], gulp.parallel('vf-scripts'));
-  //gulp.watch(['./src/pages/**/*'], gulp.series('pages','fileinclude'));
- gulp.watch(['./src/pages/**/*'], gulp.series('pages', 'fileinclude'));
- gulp.watch(['./src/pages/images/*.{svg,png,jpg,gif}'], gulp.series('build-copy'));
+  gulp.watch(['./src/pages/**/*'], gulp.parallel('build-assets', 'build-html'));
   gulp.watch(['./build/**/*'], gulp.series('browser-reload'));
 });
 
-gulp.task('set-to-development', function(done) {
-  process.argv.push('--serve');
-  fractalBuildMode = 'server';
-  done();
-});
-
-gulp.task('set-to-static-build', function(done) {
-  process.argv.push('--quiet');
-  fractalBuildMode = 'dataobject'; // run fractal in server mode as there's no need for static html assets
-  done();
-});
-
-gulp.task('fileinclude', function(done) {
-  gulp.src('./src/pages/**/*')
+gulp.task('build-html', function() {
+  return gulp.src('./src/pages/**/*.html') // important: only on html files
     .pipe(fileinclude({
       prefix: '@@',
       basepath: '@file'
     }))
     .pipe(gulp.dest(buildDestionation));
-  done();
 });
 
-// Fix to avoid corrupt images
-gulp.task('build-copy', function(done){
-  return gulp.src(['./src/pages/images/*.{svg,png,jpg,gif}'])
-    .pipe(gulp.dest(buildDestionation + '/images'));
-  done();
-});
-
-// Run build-assets, but only after we wait for fractal to bootstrap
-// @todo: consider if this could/should be two parallel gulp tasks
-gulp.task('build-assets', function(done) {
-  global.vfBuilderPath   = __dirname + '/build/vf-core-components';
-  // global.vfDocsPath      = __dirname + '/node_modules/\@visual-framework/vf-build-assets--extensions/fractal/docs';
-  global.vfOpenBrowser   = false; // if you want to open a browser tab for the component library
-  global.fractal         = require('@visual-framework/vf-core/fractal.js').initialize(fractalBuildMode,fractalReadyCallback); // make fractal components are available gloablly
-
-  function fractalReadyCallback(fractal) {
-    global.fractal = fractal; // save fractal globally
-    console.log('Done building Fractal');
-    done();
-  }
-});
-
-// Copy pages to the build directory
-gulp.task('pages', function(){
-  return gulp.src('./src/pages/**/*')
+// Copy local assets to the build directory
+gulp.task('build-assets', function() {
+  return gulp.src(['./src/pages/**/*','!./src/pages/**/*.html'])
     .pipe(gulp.dest(buildDestionation));
-
 });
 
 // Serve locally
 gulp.task('browser-sync', function(done) {
   browserSync.init({
     server: {
-          baseDir: './build',
-          index: buildDestionation+'/index.html'
-        }
+      baseDir: buildDestionation,
+      index: '/index.html'
+    }
   });
   done();
 });
@@ -119,30 +47,16 @@ gulp.task('browser-reload', function(done) {
   done();
 });
 
-
 // Let's build this sucker.
 gulp.task('build', gulp.series(
   'vf-clean',
-
-  gulp.parallel('pages','vf-css','vf-scripts','vf-component-assets'),
-  'fileinclude',
-  'set-to-static-build',
-  'build-assets',
-  'build-copy'
-
-
+  gulp.parallel('build-assets','build-html','vf-css','vf-scripts','vf-component-assets')
 ));
 
 // Build and watch things during dev
 gulp.task('dev', gulp.series(
   'vf-clean',
-
-  gulp.parallel('pages','vf-css','vf-scripts','vf-component-assets'),
-  'fileinclude',
-  'set-to-development',
-
-  'build-assets',
-  'build-copy',
+  gulp.parallel('build-assets','build-html','vf-css','vf-scripts','vf-component-assets'),
   'browser-sync',
   gulp.parallel('watch','vf-watch')
 ));
